3d3e08a0b17551beeefc548c6679453d1ea4aa9a
daviholy <david.holy225@gmail.com>
2021-05-27 22:09:55 +0200

    fixed block generating where there was no shuffling + slight refactor

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ import numpy as np
 import sys
 import secrets
 import base64
+import time
 
 ITERATIONS = 10
+DECODER = np.vectorize(lambda x: x.decode('UTF-8'))
 
 
-def xor(data: bytes, key: bytes) -> bytes:
+def xor(data: np.uint32, key: np.uint32) -> bytes:
     """xor the byte arrays of same length"""
-    key = np.frombuffer(key, np.int64)
-    data = np.frombuffer(data, np.int64)
-    xored = np.bitwise_xor(key, data)
-    return xored.tobytes()
+    return np.bitwise_xor(key, data)
 
 
 def genKey() -> bytes:
-    """generate random 256 bit long key in ascii85"""
+    """generate random 256-bit long key in ascii85"""
     tmp = np.empty(4)
     for i in range(4):
         tmp[i] = secrets.randbits(64)
@@ -27,8 +26,8 @@ def genNonce() -> np.uint32:
     return [np.uint32(secrets.randbits(32)) for _ in range(3)]
 
 
-def genKeyBlock(key: np.uint32, nonce: np.uint32, counter: np.uint32) -> bytes:
-    """generate block for the specified segment"""
+def genKeyBlock(key: np.uint32, nonce: np.uint32, counter: np.uint32) -> np.uint32:
+    """generate 512-bit block for the specified segment"""
 
     def QR(a: np.uint32, b: np.uint32, c: np.uint32, d: np.uint32) -> None:
         """chachas quarter round function"""
@@ -43,23 +42,32 @@ def genKeyBlock(key: np.uint32, nonce: np.uint32, counter: np.uint32) -> bytes:
         d = lR(d, 8)
         c += d
         b = np.bitwise_xor(b, c)
-        b = lR(7)
+        b = lR(b, 7)
+        return (a, b, c, d)
 
-    block = [np.uint32(0x65787061), np.uint32([0x6e642033]), np.uint32([0x322d6279]), np.uint32([0x7465206b]),  # "expand 32-byte k" constant
-             key[0], key[1], key[2], key[3],
-             key[4], key[5], key[6], key[7],
-             nonce[0], nonce[1], nonce[2], counter]
+    block = np.uint32([0x65787061, 0x6e642033, 0x322d6279, 0x7465206b,  # "expand 32-byte k" constant
+                       key[0], key[1], key[2], key[3],
+                       key[4], key[5], key[6], key[7],
+                       nonce[0], nonce[1], nonce[2], counter])
     for _ in range(ITERATIONS):
         # odd round
-        QR(block[0], block[4], block[8], block[12])  # column 1
-        QR(block[1], block[5], block[9], block[13])  # column 2
-        QR(block[2], block[6], block[10], block[14])  # column 3
-        QR(block[3], block[7], block[11], block[15])  # column 4
+        block[0], block[4], block[8], block[12] = QR(
+            block[0], block[4], block[8], block[12])  # column 1
+        block[1], block[5], block[9], block[13] = QR(
+            block[1], block[5], block[9], block[13])  # column 2
+        block[2], block[6], block[10], block[14] = QR(
+            block[2], block[6], block[10], block[14])  # column 3
+        block[3], block[7], block[11], block[15] = QR(
+            block[3], block[7], block[11], block[15])  # column 4
         # even round - diagonals starting from main and then going up
-        QR(block[0], block[5], block[10], block[15])
-        QR(block[1], block[6], block[11], block[12])
-        QR(block[2], block[7], block[8], block[13])
-        QR(block[3], block[4], block[9], block[14])
+        block[0], block[5], block[10], block[15] = QR(
+            block[0], block[5], block[10], block[15])
+        block[1], block[6], block[11], block[12] = QR(
+            block[1], block[6], block[11], block[12])
+        block[2], block[7], block[8], block[13] = QR(
+            block[2], block[7], block[8], block[13])
+        block[3], block[4], block[9], block[14] = QR(
+            block[3], block[4], block[9], block[14])
     return (block)
 
 
@@ -79,11 +87,17 @@ def rR(n: np.int64, d: np.int64) -> np.uint32:
 
 
 if __name__ == "__main__":
+    start = time.perf_counter()
     nonce = genNonce()
-    key = decodekey(genKey())
-    key = np.frombuffer(key, dtype=np.uint32)
-    genKeyBlock(key)
-    print(sys.argv[1])
+    key = np.frombuffer(decodekey(genKey()), np.uint32)
+    secret = np.frombuffer(bytes(
+        "Notice that this version updates each word twice, while Salsa20'", 'utf-8'), dtype=np.uint32)
+    block = genKeyBlock(key, nonce, 0)
+
+    coded = xor(secret, block)
+    print(xor(coded, block).tobytes().decode())
+    end = time.perf_counter()
+    print(end - start)
     if sys.argv[1] == "-g" or sys.argv[1] == "--generate":
         print(genkey())
     else:
